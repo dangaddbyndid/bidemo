3dcc1428e26369fd2304887fe9fca5e79e3b12db
Dan Gadd <dan.gadd@beyondidentity.com>
2021-09-20 09:14:35 +0100

    Update patch.php
    Modified Year Values

--- patch.php
+++ patch.php
@@ -3,6 +3,6 @@
 $greeting = "Hello!";
 // Assign the value 8 to the variable "month"
 $month = 8;
-// Assign the value 2019 to the variable "year"
-$year = 2019;
+// Assign the value 2021 to the variable "year"
+$year = 2021;
 ?>
