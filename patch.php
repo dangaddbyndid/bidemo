15622283be73ad936386cba668e704a712ebb1a9
Dan Gadd <dan.gadd@beyondidentity.com>
2021-09-20 09:09:03 +0100

    Update patch.php

--- patch.php
+++ patch.php
@@ -1,2 +1,8 @@
-dsada
-new line of codes
+<?php
+// Assign the value "Hello!" to the variable "greeting"
+$greeting = "Hello!";
+// Assign the value 8 to the variable "month"
+$month = 8;
+// Assign the value 2019 to the variable "year"
+$year = 2019;
+?>
